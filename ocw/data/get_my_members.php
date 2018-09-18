4fc151845386d14b842d5fed211e060210c1e64d
mash0824 <andrew.pe@gmail.com>
2018-09-18 16:54:57 +0800

    test

--- ocw/data/get_my_members.php
+++ ocw/data/get_my_members.php
@@ -2,11 +2,13 @@
 require(dirname(dirname(__FILE__))."/includes.php");
 
 $mymembers = getClanMembers($con, $_SESSION['my_clan']['clan_id']);
+
 $s = array();
-$s['data'] = utf8ize($mymembers);
+$s['data'] = $mymembers;
+
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-echo json_encode($s);
+echo json_encode($s, JSON_UNESCAPED_UNICODE);
 exit;
 ?>
