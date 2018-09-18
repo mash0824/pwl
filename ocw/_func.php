e44033fe89b7784210e735ce5646b7b3b1636ba1
mash0824 <andrew.pe@gmail.com>
2018-09-18 16:51:25 +0800

    test

--- ocw/_func.php
+++ ocw/_func.php
@@ -39,6 +39,17 @@ function getTeamLists($db, $id=""){
     return $return;
 }
 
+function utf8ize($mixed) {
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } else if (is_string ($mixed)) {
+        return utf8_encode($mixed);
+    }
+    return $mixed;
+}
+
 function getClanMembers($db, $id=""){
     $return = array();
     $sql = "SELECT * FROM clan_members";
--- ocw/data/get_my_members.php
+++ ocw/data/get_my_members.php
@@ -3,12 +3,10 @@ require(dirname(dirname(__FILE__))."/includes.php");
 
 $mymembers = getClanMembers($con, $_SESSION['my_clan']['clan_id']);
 $s = array();
-$s['data'] = $mymembers;
-echo 1;
+$s['data'] = utf8ize($mymembers);
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 echo json_encode($s);
-echo json_last_error();
 exit;
 ?>
