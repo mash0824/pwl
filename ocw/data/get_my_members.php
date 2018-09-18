3c0c3b6c34534062424aeb94cc9c6de7f7b44bbe
mash0824 <andrew.pe@gmail.com>
2018-09-18 16:56:42 +0800

    test

--- ocw/data/get_my_members.php
+++ ocw/data/get_my_members.php
@@ -10,5 +10,6 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 echo json_encode($s, JSON_UNESCAPED_UNICODE);
+echo json_last_error();
 exit;
 ?>
