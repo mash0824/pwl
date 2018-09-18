5dc367171f0e83add77a93e3499e497645afa1df
mash0824 <andrew.pe@gmail.com>
2018-09-18 17:06:04 +0800

    test

--- ocw/includes.php
+++ ocw/includes.php
@@ -3,3 +3,10 @@ require(dirname(__FILE__)."/_config.php");
 require(dirname(__FILE__)."/_func.php");
 require(dirname(__FILE__)."/_f_curl_v1.6.php");
 $con = new mysqli(db_host, db_user, db_password,db_name);
+if (!$con->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $con->error);
+    exit();
+} else {
+    printf("Current character set: %s\n", $con->character_set_name());
+}
+
