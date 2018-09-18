f0750b60384e12b99818829306cc88559a249042
mash0824 <andrew.pe@gmail.com>
2018-09-18 16:39:25 +0800

    test

--- ocw/data/get_my_members.php
+++ ocw/data/get_my_members.php
@@ -5,6 +5,9 @@ $mymembers = getClanMembers($con, $_SESSION['my_clan']['clan_id']);
 $s = array();
 $s['data'] = $mymembers;
 
-echo json_encode($s, JSON_NUMERIC_CHECK);
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+echo json_encode($s);
 exit;
 ?>
